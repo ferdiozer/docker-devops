6b253d5605de733b2dca97c4d509ca131285c99b
Ferdi Özer <ferdi.ozer@topraq.ai>
2024-01-29 23:05:30 +0300

    rebbitmq

---
+++ examples/rabbitmq_listener.js
@@ -0,0 +1,24 @@
+  
+  
+  const amqplib = require('amqplib');
+
+  (async () => {
+    const queue = 'tasks';
+    const conn = await amqplib.connect('amqp://localhost');
+  
+    const ch1 = await conn.createChannel();
+    await ch1.assertQueue(queue);
+  
+   // Listener
+   ch1.consume(queue, (msg) => {
+    if (msg !== null) {
+      console.log('Recieved:', msg.content.toString(),new Date());
+      ch1.ack(msg);
+    } else {
+      console.log('Consumer cancelled by server');
+    }
+  });
+  
+  })();
+  
+
